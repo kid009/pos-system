7f3bba59a2c7fc9a2e1440e97ab531280aa62a6b
kid009 <pongpoom1985@gmail.com>
2025-12-01 23:01:52 +0700

    updated new app

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\HomeController;
-use App\Http\Controllers\Admin\ProductCategoryController;
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::get('home', [HomeController::class, 'index'])->name('home');
-        Route::resource('product-categories', ProductCategoryController::class);
-    });
-
-    
-});
-
-require __DIR__.'/auth.php';
