471e21ce5c4adc87ed199c13e52e0e9f116ae68a
kid009 <pongpoom1985@gmail.com>
2025-12-20 10:41:32 +0700

    ประกาศตัวแปร search

--- app/Livewire/Customer/CustomerIndex.php
+++ app/Livewire/Customer/CustomerIndex.php
@@ -10,7 +10,7 @@ class CustomerIndex extends Component
 {
     use WithPagination;
 
-    protected $search = '';
+    public string $search = '';
 
     public function updatedSearch()
     {
@@ -25,15 +25,22 @@ class CustomerIndex extends Component
 
     public function render()
     {
+        // dd($this->search);
         $customers = Customer::query()
-            ->where('name', 'like', "%{$this->search}%")
-            ->orWhere('code', 'like', "%{$this->search}%")
-            ->orWhere('phone', 'like', "%{$this->search}%")
-            ->orderBy('created_at', 'desc')
+            ->when($this->search, function ($query) {
+                // สร้างวงเล็บครอบเงื่อนไข OR ไว้ (Group) เพื่อไม่ให้ตีกับเงื่อนไขอื่น
+                $query->where(function ($subQuery) {
+                    $subQuery->where('name', 'like', '%' . $this->search . '%')
+                             ->orWhere('code', 'like', '%' . $this->search . '%')
+                             ->orWhere('phone', 'like', '%' . $this->search . '%')
+                             ->orWhere('line_id', 'like', '%' . $this->search . '%');
+                });
+            })
+            ->orderBy('created_at', 'desc') // เรียงจากใหม่ไปเก่า
             ->paginate(10);
 
         return view('livewire.customer.customer-index', [
-            'customers' => $customers,
+            'customers' => $customers
         ]);
     }
 }
